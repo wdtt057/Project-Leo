759b0a7331b40bfe048e1e1a07d2f667db434be2
Joshua Mejia <jmejia7951@gmail.com>
2020-09-28 21:34:26 -0700

    Adding frameworks and setting up base layout

---
+++ resources/js/components/navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar is-dark">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img title="" src="" alt="">
+          Project LEO
+        </a>
+        <span class="navbar-burger burger" :class="{'is-active':isActive}" @click="burgerToggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div id="navMenu" class="navbar-menu" :class="{'is-active':isActive}">
+        <div class="navbar-end">
+          <a href="#" class="navbar-item button is-primary">Sign Up</a>
+          <a href="#" class="navbar-item">About</a>
+          <a href="#" class="navbar-item">Pricing</a>
+          <!--<a href="/store" class="navbar-item">Bait Store</a>-->
+          <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Resources</a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Link 1</a>
+            <a class="navbar-item" href="https://www.youtube.com/watch?v=ZZ5LpwO-An4" target="_blank">Link 2</a>
+            <hr class="navbar-divider">
+            <a class="navbar-item" href="/" style="color:#41CAE5; font-weight: bold;">Contact Us</a>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+  methods: {
+    burgerToggle(){
+      this.isActive  = !this.isActive;
+    }
+  }
+}
+</script>
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Project LEO</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link rel="stylesheet" href="\css\app.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+    <script defer src="\js\app.js"></script>
+  </head>
+  <body>
+      <div id="app">
+        <navbar></navbar>
+        <div class="container">
+          @yield('content')
+        </div>
+      </div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Copyright &copy; 2020 - All Rights reserved - Project LEO
+      </p>
+    </div>
+  </footer>
+  </body>
+</html>
