5cda937a09b5101e94ee48f570b19ba74d04c1f4
Ryan Catterson <theryancatterson@gmail.com>
2020-10-08 19:44:08 -0700

    whatever

---
+++ resources/js/components/aboutus.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+         <section class="section is-small" id="PersonA">
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <figure class="image is-1by1">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth is-offset-two-fifths">
+                    <h2 class="title is-4">&lt;Person 1&gt;</h2>
+                    <ul>
+                        <li>&bull;blah</li>
+                        <li>&bull;blah blah</li>
+                        <li>&bull;blah blah blah</li>
+                    </ul>
+                </div> 
+            </div>
+        </section>
+        <section class="section is-small" id="PersonB">
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <h2 class="title is-4">&lt;Person 2&gt;</h2>
+                    <ul>
+                        <li>&bull;blah</li>
+                        <li>&bull;blah blah</li>
+                        <li>&bull;blah blah blah</li>
+                    </ul>
+                </div>
+                <div class="column is-one-fifth is-offset-two-fifths">
+                    <figure class="image is-1by1">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="section is-small" id="PersonC">
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <figure class="image is-1by1">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth is-offset-two-fifths">
+                    <h2 class="title is-4">&lt;Person 3&gt;</h2>
+                    <ul>
+                        <li>&bull;blah</li>
+                        <li>&bull;blah blah</li>
+                        <li>&bull;blah blah blah</li>
+                    </ul>
+                </div> 
+            </div>
+        </section>
+        <section class="section is-small" id="PersonD">
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <h2 class="title is-4">&lt;Person 4&gt;</h2>
+                    <ul>
+                        <li>&bull;blah</li>
+                        <li>&bull;blah blah</li>
+                        <li>&bull;blah blah blah</li>
+                    </ul>
+                </div> 
+                <div class="column is-one-fifth is-offset-two-fifths">
+                    <figure class="image is-1by1">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="section is-small" id="PersonE">
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <figure class="image is-1by1">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth is-offset-two-fifths">
+                    <h2 class="title is-4">&lt;Person 5&gt;</h2>
+                    <ul>
+                        <li>&bull;blah</li>
+                        <li>&bull;blah blah</li>
+                        <li>&bull;blah blah blah</li>
+                    </ul>
+                </div> 
+            </div>
+            
+        </section>
+    </div>
