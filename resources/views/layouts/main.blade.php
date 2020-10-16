bd1407dad78d3d0941318c419529944740f2e11e
Joshua Mejia <jmejia7951@gmail.com>
2020-10-15 21:51:32 -0700

    Converting dashboard navbar into vue component

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,45 +14,6 @@
 <body>
 	<div id="app">
   <navbardash :user="{{ Auth::user() }}" route="{{ route('logout') }}" csrf="{{csrf_token()}}"></navbardash>
-<!-- 	<nav class="navbar is-dark">
-    <div class="container">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/home">
-					<i class="fas fa-home"></i>&nbsp;Home
-        </a>
-        <span class="navbar-burger burger" :class="{'is-active':isActive}" @click="burgerToggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      </div>
-    <div id="navMenu" class="navbar-menu" :class="{'is-active':isActive}">
-      <div class="navbar-start">
-          !-- <a href="#" class="navbar-item">Pricing</a> --
-      </div>
-        <div class="navbar-end">
-					<a href="/progress" class="navbar-item">Progress&nbsp;<i class="far fa-chart-bar"></i></a>
-					<div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">{{ Auth::user()->name }}</a>
-            <div class="navbar-dropdown">
-              <a class="navbar-item" href="#"><i class="fas fa-user"></i>&nbsp;Profile</a>
-              <a class="navbar-item" href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
-							<a href="#" class="navbar-item"><i class="far fa-question-circle"></i>&nbsp;Help</a>
-              <hr class="navbar-divider">
-              <a class="navbar-item" href="{{ route('logout') }}" style="color:red; font-weight: bold;"
-							onclick="event.preventDefault();
-								document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
-							{{ __('Logout') }}
-							</a>
-							<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-								@csrf
-							</form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav> -->
 			<main class="py-4">
 					@yield('content')
 			</main>
