fbff1d46a2d71c1e7f2f569598e5ee28bd46c29f
Dengtai Wang <wdt296495116@outlook.com>
2020-10-28 10:12:12 +0800

    course database created

---
+++ database/migrations/2020_10_26_035515_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("course_name");
+            $table->string("language_type"); //php,CSS,JS...
+            $table->double("difficulty_level", 1, 1); //0.0 to 9.9 add 0.1 then go 0.1 to 10.0
+            $table->longText('description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
