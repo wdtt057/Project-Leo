20be617b7fc2fa59d02069e6e271385671805e83
Dengtai Wang <wdt296495116@outlook.com>
2020-10-28 15:35:29 +0800

    quiz table

--- database/migrations/2020_10_28_023637_create_quiz_table.php
+++ database/migrations/2020_10_28_023637_create_quiz_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCoursesTable extends Migration
+class CreateQuizTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateCoursesTable extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
+        Schema::create('quiz', function (Blueprint $table) {
             $table->id();
-            $table->string("course_name");
+            $table->string("quiz_name");
             $table->string("language_type"); //php,CSS,JS...
             $table->double("difficulty_level", 1, 1); //0.0 to 9.9 add 0.1 then go 0.1 to 10.0
-            $table->longText('description')->nullable();
+            $table->integer('score');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -31,6 +31,6 @@ class CreateCoursesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('quiz');
     }
 }
