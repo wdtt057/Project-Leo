be72937215b3320e908f3088b3362e382d023097
Josh Mejia <jmejia7951@gmail.com>
2020-10-30 14:39:38 -0700

    Added Form Validation for Registration page and Password Recovery Page

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,7 +9,9 @@
     <link rel="stylesheet" href="\css\app.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
-    <script defer src="\js\app.js"></script>
+    <script defer src="/js/app.js"></script>
+    <script defer src="/js/form-validation.js"></script>
+    <script src="/js/jquery.min.js"></script>
   </head>
   <body>
     <div id="app">
