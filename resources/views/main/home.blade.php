19e60922a81da8fb523d8714dce10cd1371bb540
Josh Mejia <jmejia7951@gmail.com>
2020-10-30 13:19:55 -0700

    Converted Dash Aside menu into vue component. Also renamed all Vue components to follow best practice

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -3,47 +3,7 @@
 @section('content')
 <div class="section">
   <div class="columns">
-    <aside class="column is-2">
-      <nav class="menu">
-        <p class="menu-label">
-          General
-        </p>
-        <ul class="menu-list">
-          <li><a class="">Dashboard <i class="far fa-chart-bar"></i></a></li>
-        </ul>
-        <p class="menu-label">
-          Courses
-        </p>
-        <ul class="menu-list">
-          <li><a>Introduction</a></li>
-          <li>
-            <a id="HTML" class="has-submenu">HTML</a>
-            <ul id="submenuHTML" class="test">
-              <li><a href="/courses/html/level1/lesson1">Lesson 1</a></li>
-              <li><a>Lesson 2</a></li>
-              <li><a>Lesson 3</a></li>
-            </ul>
-          </li>
-          <li>
-            <a id="CSS" class="has-submenu">CSS</a>
-            <ul id="submenuCSS" class="test">
-              <li><a>Lesson 1</a></li>
-              <li><a>Lesson 2</a></li>
-              <li><a>Lesson 3</a></li>
-            </ul>
-          </li>
-          <li>
-            <a id="JS" class="has-submenu">JavaScript</a>
-            <ul id="submenuJS" class="test">
-              <li><a>Lesson 1</a></li>
-              <li><a>Lesson 2</a></li>
-              <li><a>Lesson 3</a></li>
-            </ul>
-          </li>
-        </ul>
-        <a class="menu-list" href="/FAQs"><i class="far fa-question-circle"></i>&nbsp;Help Center</a>
-      </nav>
-    </aside>
+    <aside-menu></aside-menu>
     <main class="column">
       <div class="level">
         <div class="level-left">
@@ -210,7 +170,7 @@
 </script>
 
 <style>
-.test {
+.submenu-list {
   max-height: 0;
   overflow:hidden;
   transition: all 0.2s ease-in-out;
