6109cd3f49c0cf35f6d5f14e09dad8b362bcbcee
Josh Mejia <jmejia7951@gmail.com>
2020-11-06 14:24:09 -0800

    Quiz Version 1.0 - uses AJAX request with JSON

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <script src="\js\jquery.min.js"></script> 
   </head>
   <body>  
   <div class="box">
@@ -13,9 +14,21 @@
     <a id="exitQuiz" class="button is-link is-hidden" href="/home">Exit Quiz</a>
     <button id="retryQuiz" class="button is-danger is-hidden" onclick="window.location.reload();">Retry Quiz</button>
   </div>
-  <!--<script defer src="/js/quiz/quiz.js"></script>-->
-  <script src="/js/quiz/html-q1.js"></script>
   <script>
+  // get JSON data
+  var jsonData = (function() {
+  var json = null;
+  $.ajax({
+      'async': false,
+      'global': false,
+      'url': "js/quiz/html-q1.json",
+      'dataType': "json",
+      'success': function(data) {
+        json = data;
+      }
+    });
+    return json;
+  })();
   //json array
   var i = 0;
   var correctCount = 0;
