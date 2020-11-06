e564253b336e773f3672f55ac80788173f47ee30
Dengtai Wang <wdt296495116@outlook.com>
2020-11-06 11:22:08 +0800

    quiz chart

--- public/js/home.js
+++ public/js/home.js
@@ -37,45 +37,47 @@ var myChart = new Chart(lessonsCompleted, {
     }
 });
 //Quiz Scores Chart
-var quizScores = document.getElementById('quizScores').getContext('2d');
-var myLineChart = new Chart(quizScores, {
-    'type': 'line',
-    'data': {
-        'labels': ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5'],
-        'datasets': [
-            {
-            'label': 'HTML Score',
-            'data': [100, 80, 75, 90, 60],
-            'fill': false,
-            'borderColor': 'rgba(255, 99, 132, 0.7)',
-            'lineTension': 0.1
-            },
-            {
-            'label': 'CSS Score',
-            'data': [60, 75, 90, 100, 83],
-            'fill': false,
-            'borderColor': 'rgba(255, 206, 86, 0.7)',
-            'lineTension': 0.1
-            },
-            {
-            'label': 'JavaScript Score',
-            'data': [100, 90, 90, 100, 55],
-            'fill': false,
-            'borderColor': 'rgba(153, 102, 255, 0.7)',
-            'lineTension': 0.1
-            }
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                display: true,
-                ticks: {
-                    suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
-                    // OR //
-                    beginAtZero: true   // minimum value will be 0.
+$.get('get_html_data', function (data, status) {
+    var quizScores = document.getElementById('quizScores').getContext('2d');
+    var myLineChart = new Chart(quizScores, {
+        'type': 'line',
+        'data': {
+            'labels': ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5'],
+            'datasets': [
+                {
+                    'label': 'HTML Score',
+                    'data': data,
+                    'fill': false,
+                    'borderColor': 'rgba(255, 99, 132, 0.7)',
+                    'lineTension': 0.1
+                },
+                {
+                    'label': 'CSS Score',
+                    'data': [60, 75, 90, 100, 83],
+                    'fill': false,
+                    'borderColor': 'rgba(255, 206, 86, 0.7)',
+                    'lineTension': 0.1
+                },
+                {
+                    'label': 'JavaScript Score',
+                    'data': [100, 90, 90, 100, 55],
+                    'fill': false,
+                    'borderColor': 'rgba(153, 102, 255, 0.7)',
+                    'lineTension': 0.1
                 }
-            }]
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                        // OR //
+                        beginAtZero: true   // minimum value will be 0.
+                    }
+                }]
+            }
         }
-    }
+    })
 });
--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <div class="section">
   <div class="columns">
     <aside class="column is-2">
