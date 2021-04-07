448cc5a7658c24f9d9c4aade45a3a7c3dc141fb5
Elizabeth Barbieto <ekbarbieto17@gmail.com>
2021-04-06 21:09:41 -0700

    update about us

--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
-        <section class="section has-text-centered is-small">
-            <h1 class="title is-1">Our Mission</h1>
-            <h2>With Project-Leo, our mission is to empower students by teaching Web Development techniques that will allow them to grow and apply themselves.</h2>
-            <br><img class="is-rounded" src="\images\background_img_1.png" width="400px" height="400px">
+        <section class="box section is-small">
+            <h1 class="title is-1 has-text-centered ">Our Mission</h1>
+            <h2 class="container is-one-fifth" style="width:775px;"><strong>Project L.E.O (Learning Envirnment Online) is a free course made to teach kids how to create their own websites. Web design is not only a great creative outlet, but also a valuable life skill that can be used in future careers. Furthermore, learning to code is a great introduction to learning how to problem solve and think logically! Unfortunately, coding is typically not taught in most schools and valuable time to practice is lost. The sooner you start your journey in the wonderful world of programming, the better! Thus Project L.E.O is here as an open window into your bright future.</strong>
+            <br><br><img class="top-image is-rounded" src="\images\background_img_1.png" width="400px" height="400px">
+            </h2>
         </section>
+        <br><br>
         <h1 class="title has-text-centered">Team Members</h1>
         <div class="team columns is-centered">
             <div class="column has-text-centered is-2">
-                <img class="is-rounded" src="\images\joshua_icon.png" width="150px" height="150px">
+                <img class="is-rounded" src="\images\josh_icon.png" width="150px" height="150px">
                 <h2>Joshua Mejia</h2>
                 <h3>Full Stack Developer</h3>
             </div> 
             <div class="column has-text-centered is-2">
-                <img class="is-rounded" src="\images\elizabeth_with_dog_icon.png" width="150px" height="150px">
+                <img class="is-rounded" src="\images\elizabeth_icon.png" width="150px" height="150px">
                 <h2>Elizabeth Barbieto</h2>
                 <h3>UI Designer / Frontend Developer</h3>
             </div> 
             <div class="column has-text-centered is-2">
-                <img class="is-rounded" src="\images\ryan_with_dog_icon.png" width="150px" height="150px">
+                <img class="is-rounded" src="\images\ryan_icon.png" width="150px" height="150px">
                 <h2>Ryan Catterson</h2>
                 <h3>Frontend Developer</h3>
             </div> 
@@ -35,3 +37,11 @@
         </div>
     </div>
 </template>
+
+<style>
+.top-image{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
